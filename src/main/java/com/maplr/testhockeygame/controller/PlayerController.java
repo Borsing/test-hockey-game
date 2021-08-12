9e515d419d6c2962921330bfa415a1c88a1a0c34
Benjamin Robert <benjamin.robert@cbp-group.com>
2021-08-12 11:20:11 +0200

    :memo: Add swagger documentation on endpoints.

--- src/main/java/com/maplr/testhockeygame/controller/PlayerController.java
+++ src/main/java/com/maplr/testhockeygame/controller/PlayerController.java
@@ -3,6 +3,8 @@ package com.maplr.testhockeygame.controller;
 import com.maplr.testhockeygame.dto.PlayerDto;
 import com.maplr.testhockeygame.mapper.PlayerMapper;
 import com.maplr.testhockeygame.service.PlayerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/player")
+@Api(tags = "Player Service")
 @RestController
 @RequiredArgsConstructor
 public class PlayerController {
@@ -18,6 +21,7 @@ public class PlayerController {
     private final PlayerMapper playerMapper;
 
     @PutMapping("/captain/{number}")
+    @ApiOperation("Set a player as new captain of his team")
     public PlayerDto becomeCaptainOfTeam(@PathVariable("number") final Long number) {
         var newCaptain = playerService.becomeCaptainOfTeam(number);
         return playerMapper.toPlayerDto(newCaptain);
