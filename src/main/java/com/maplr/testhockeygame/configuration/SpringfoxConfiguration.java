437e64aa88ada9765b94af078d279bc7c6f23696
Benjamin Robert <benjamin.robert@cbp-group.com>
2021-08-12 03:23:21 +0200

    :sparkles: Add swagger documentation.

---
+++ src/main/java/com/maplr/testhockeygame/configuration/SpringfoxConfiguration.java
@@ -0,0 +1,29 @@
+package com.maplr.testhockeygame.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringfoxConfiguration {
+
+    @Bean
+    public Docket documentationApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.maplr.testhockeygame"))
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("TestHockeyGame")
+                        .description("TestHockeyGame")
+                        .version("0.0.1")
+                        .build());
+    }
+
+}
